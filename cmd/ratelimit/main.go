f5164698034d7107839fb14b38781c710372fe1b
Mehul Prajapati <ailearn90@gmail.com>
2026-08-01 06:21:21 +0530

    token bucket & per client limiting

---
+++ cmd/ratelimit/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/MehulxBuilds/go-ratelimiter/internal/service"
+)
+
+func main() {
+
+	// Request Handler
+	handler := service.EndpointHandler
+
+	// Middlewares for specific rate limiting strategies
+	bucketMiddleware := service.BucketRateLimiter
+	perClientMiddleware := service.PerClientRateLimiter
+
+	// Impl Token Bucket
+	http.Handle("/ping/bucket", bucketMiddleware(handler))
+
+	// Impl Per-Client Limiting
+	http.Handle("/ping/per-client", perClientMiddleware(handler))
+	
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("There was an error listening on port :8080", err)
+	}
+}
---
+++ internal/service/limiter.go
@@ -0,0 +1,107 @@
+package service
+
+import (
+	"encoding/json"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type Message struct {
+	Status string `json:"status"`
+	Body   string `json:"body"`
+}
+
+func EndpointHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	
+	message := Message{
+		Status: "Successful",
+		Body: "Hi! You've reached the API. How may I help you?",
+	}
+	err := json.NewEncoder(writer).Encode(&message)
+	if err != nil {
+		return
+	}
+}
+
+func BucketRateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			message := Message {
+				Status: "Request Failed",
+				Body: "The API is at capacity, try again later",
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		} else {
+			next(w, r)
+		}
+	})
+}
+
+func PerClientRateLimiter(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			// Lock the mutex to protect this section from race conditions.
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+
+			message := Message {
+				Status: "Request Failed",
+				Body: "The API is at capacity, try again later",
+			}
+
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		}
+		mu.Unlock()
+		next(w, r)
+	})
+}
